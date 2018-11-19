a2c0fb132283d3b4ded851c512f01ab91380daa3
Tom Busby <tom@busby.ninja>
2018-11-19 16:00:09 +0100

    Show pretty-printed JSON and demonstrate errors

--- unjson-examples.hs
+++ unjson-examples.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Aeson
-import Data.Text as Text
+import Data.Aeson hiding (Options)
+import Data.ByteString.Lazy.Char8 (unpack)
+import Data.Text as Text hiding (unpack)
 import Data.Unjson
 
 data Example = Example
@@ -22,18 +23,39 @@ unjsonExample = objectOf $ pure Example
           exampleOptional
           "Optional boolean"
 
-doparse :: Data.Unjson.Result Example
-doparse = parse unjsonExample $ object 
+prettyOptions :: Options
+prettyOptions =  Options { pretty = True, indent = 4, nulls = False}
+
+objectNoErrors :: Value
+objectNoErrors = object
     [ "name" .= ("test" :: Text.Text)
-    , "array_of_ints" .= 
+    , "array_of_ints" .=
         [ toJSON (123 :: Int)
         , toJSON (321 :: Int)
         ]
     , "optional_bool" .= True
     ]
 
+objectWithErrors :: Value
+objectWithErrors = object
+    [ "name" .= (666 :: Int)
+    , "array_of_ints" .=
+        [ toJSON ("unexpected string" :: Text.Text)
+        , toJSON (321 :: Int)
+        ]
+    , "optional_bool" .= True
+    ]
+
+doparse :: Value -> Data.Unjson.Result Example
+doparse obj = parse unjsonExample obj
+
 main = do
-    let Result val iss = doparse
-    print $ exampleArray val  -- This shows the parsed Aeson AST.
-    print iss                 -- This will print any parse errors that occurred if any.
-    print $ unjsonToByteStringLazy unjsonExample val -- Turns an Aeson AST into a JSON string
+    let Result val iss = doparse objectNoErrors
+    let Result val_e iss_e = doparse objectWithErrors
+    print $ exampleArray val   -- This shows the parsed Aeson AST.
+    print iss_e                -- This will print any parse errors that occurred if any.
+    putStrLn $ toBSunpack  val -- Turns an Aeson AST into a JSON string
+    putStrLn $ toBSunpack' val -- Turns an Aeson AST into a JSON string (with pretty print)
+        where
+            toBSunpack = unpack . unjsonToByteStringLazy unjsonExample
+            toBSunpack' = unpack . unjsonToByteStringLazy' prettyOptions unjsonExample
